35652bcfaa05295cfbb8c12ecbe34eda6f1591d8
Lewis Daly <lewisdaly@me.com>
2023-03-23 13:53:53 +0200

    debug: custom module

--- synapse/modules/cognito/cognito.py
+++ synapse/modules/cognito/cognito.py
@@ -1,7 +1,7 @@
 import json
 import logging
 
-
+from synapse.http.servlet import parse_string
 from twisted.web.resource import Resource
 from twisted.web.server import Request
 
@@ -15,24 +15,33 @@ class DemoResource(Resource):
         super(DemoResource, self).__init__()
         self.config = config
 
-    def render_GET(self, request: Request):
-        logger.info("/_synapse/client/demo/hello")
-        print('hello world')
-        # name = request.args.get(b"name")[0]
+    def render_POST(self, request: Request):
+        logger.info("/_synapse/modules/cognito/login")
+
+        # get the access_token and id_token from request
+        access_token = parse_string(request, "access_token", required=True)
+        id_token = parse_string(request, "id_token", required=True)
+
+        # TODO: parse the token, make sure it is valid based on:
+        # - cognito client's public key (inject from Env Var)
+        # - access token hasn't expired
+        # - other security???
+
+        # TODO: internal call to get the login token
 
         request.setHeader(b"Content-Type", b"application/json")
-        return json.dumps({"hello": True}).encode('utf-8')
+        return json.dumps({"login_token": "12345"}).encode('utf-8')
     
 class Cognito:
     def __init__(self, config: dict, api: ModuleApi):
         logger.info("Starting Cognito module")
-        print("Starting Cognito module")
+        print("Starting Cognito module2")
 
         self.config = config
         self.api = api
 
         self.api.register_web_resource(
-            path="/_synapse/client/demo/hello",
+            path="/_synapse/modules/cognito/login",
             resource=DemoResource(self.config),
         )
 
